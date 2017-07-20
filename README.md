dc66e41ae1044c9f42bad4209162d0e68e1fa6fe
jacobo <info@jacobo.tech>
2017-07-20 22:21:54 +0100

    ready for example deployment

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+![Travis CI status](https://travis-ci.org/elmarti/meteorantdboilerplate.svg?branch=master)
+
+[Example site](https://antdmeteor.herokuapp.com)
+Admin account: admin@antdexample.com password:password
 # Meteor AntD boilerplate
 
 I keep using this setup for side projects so I've decided to build a boilerplate project to work from.
--- server/startup.js
+++ server/startup.js
@@ -3,35 +3,37 @@
 // import { Roles } from 'meteor/alanning:roles';
 // import { Random } from 'meteor/random';
 //
-// Meteor.startup(() => {
-//     console.log('running data init');
-//     if(!Meteor.users.findOne()){
-//         console.log('No users found, creating admin');
-//         const userId = Accounts.createUser({
-//             email: 'admin@antdexample.com',
-//             password: 'password'
-//         });
-//         console.log('Admin user created, _id: %s', userId);
-//         Roles.addUsersToRoles(userId, ['admin','user'], Roles.GLOBAL_GROUP);
-//     }
+Meteor.startup(() => {
+    Accounts.onCreateUser((options, user)=>{
+        user["profile"] = {
+            createdAt: new Date()
+        };
+        return user;
+    });
+    console.log('running data init');
+    if(!Meteor.users.findOne()){
+        console.log('No users found, creating admin');
+        const userId = Accounts.createUser({
+            email: 'admin@antdexample.com',
+            password: 'password'
+        });
+        console.log('Admin user created, _id: %s', userId);
+        Roles.addUsersToRoles(userId, ['admin','user'], Roles.GLOBAL_GROUP);
+
+
+        let i = 0;
+        while(i<100){
+            console.log('adding user');
+            const userId = Accounts.createUser({
+                email: Random.id() + '@' +  Random.id(),
+                password: 'password'
+            });
+            console.log('Random user created, _id: %s', userId, i );
+            Roles.addUsersToRoles(userId, 'user', Roles.GLOBAL_GROUP);
+            i++
+        }
+    }
 //
-//     Accounts.onCreateUser((options, user)=>{
-//         user["profile"] = {
-//             createdAt: new Date()
-//         };
-//        return user;
-//     });
+
 //
-//     let i = 0;
-//     while(i<100){
-//         console.log('adding user');
-//         const userId = Accounts.createUser({
-//             email: Random.id() + '@' +  Random.id(),
-//             password: 'password'
-//         });
-//         console.log('Random user created, _id: %s', userId, i );
-//         Roles.addUsersToRoles(userId, 'user', Roles.GLOBAL_GROUP);
-//         i++
-//     }
-//
-// });
+});
