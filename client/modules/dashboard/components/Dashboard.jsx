b425977d8c295d0b5e7ae8364c562dd5d6f25d81
jacobo <info@jacobo.tech>
2017-07-21 07:18:50 +0100

    make user dashboard selection reactive

--- client/modules/dashboard/components/Dashboard.jsx
+++ client/modules/dashboard/components/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Roles } from 'meteor/alanning:roles';
+import { createContainer } from 'meteor/react-meteor-data';
 import { Admin, User } from '../';
-export default () =>{
-    if(Roles.userIsInRole(Meteor.userId(), 'admin', Roles.GLOBAL_GROUP))
-        return <Admin/>;
-    else return <User/>;
+class Dashboard extends React.Component{
+    render(){
+        if(this.props.isAdmin)
+            return <Admin/>;
+        else return <User/>;
 
-};
+    }
+}
+
+
+export default createContainer(() => {
+    const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin', Roles.GLOBAL_GROUP);
+    return {
+        isAdmin
+    };
+}, Dashboard);
